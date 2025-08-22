3cdd080193b4c133723f0b5dd7e8cbcb92393e83
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-08-21 23:06:34 -0300

    Atualização da home

--- pages/home.py
+++ pages/home.py
@@ -2,17 +2,19 @@ import streamlit as st
 from os import sep
 
 st.set_page_config(
-    page_title="Home",
+    page_title="Home"
 )
 
 st.title("Curso - Ciência de dados - EBAC")
 
 st.html(
     """
-    <p>Boas-vindas ao meu portfólio de projetos do curso de Ciência de Dados da EBAC!</p>
-    <div style="display: flex; align-items: center; gap: 10px;">
+    <div style="display: flex; align-items: flex-start; gap: 10px;">
         <img style="height: 100px" src="app/static/hi-otag.gif"/>
-        <p style="margin: 0; text-align: justify"> Aqui você encontrará uma seleção dos trabalhos que desenvolvi ao longo do curso, abordando temas como análise de dados, visualização, estatística aplicada e <i>machine learning</i>.</p>
+        <div>
+            <p style="margin: 0;"><b>Olá!</b><br>Boas-vindas ao meu portfólio de projetos do curso de <b>Ciência de Dados</b> da EBAC!</b></p>
+            <p style="margin: 0; text-align: justify"> Aqui você encontrará uma seleção dos trabalhos que desenvolvi ao longo do curso, abordando temas como <b>análise de dados</b>, <b>visualização</b>, <b>estatística aplicada</b> e <b><i>machine learning</i></b>.</p>
+        </div>
     </div>
     """
 )
